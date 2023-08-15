1c2950f8ece6f0d268b742596365643de6c50839
Ralph Reyes <ralph.reyes@etradingsoftware.com>
2023-08-15 12:45:22 +0800

    updated navigation behavior to overflow outside viewport in a smaller screen size and enable swiping/touch functionality

--- index.js
+++ index.js
@@ -26,3 +26,32 @@ const navigateTo = (e) => {
 navLinks.forEach(nav => {
   nav.addEventListener('click', navigateTo);
 })
+
+// slider will only work if CSS properties have been set accordingly based on screen size
+
+const slider = document.querySelector('.navigation');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
